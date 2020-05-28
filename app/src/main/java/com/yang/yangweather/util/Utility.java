3c5ce1044ebc727db8b25f78ba836d93f3998789
melertoyyang <54160539@qq.com>
2020-05-28 15:38:44 +0800

    "加入显示天气信息的功能"

--- app/src/main/java/com/yang/yangweather/util/Utility.java
+++ app/src/main/java/com/yang/yangweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.yang.yangweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.yang.yangweather.db.City;
 import com.yang.yangweather.db.County;
 import com.yang.yangweather.db.Province;
+import com.yang.yangweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -74,5 +76,18 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+            } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 }
